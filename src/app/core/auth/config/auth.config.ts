bd9a5d0d433d098e310a9a8b44fabe338d1ae78b
ckalmar <ckalmar@craneware.com>
2025-02-19 11:29:07 +0000

    Auth0 fixes

---
+++ src/app/core/auth/config/auth.config.ts
@@ -0,0 +1,7 @@
+export const authConfig = {
+    domain: 'kalmardev.uk.auth0.com',
+    clientId: 'pYjVDw6PRDNIfOroAztK0GaXNKmT3u0o',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AuthModule } from '@auth0/auth0-angular';
+import { authConfig } from './app/core/auth/config/auth.config';
+import { HttpClientModule } from '@angular/common/http';
+
+const appConfig = {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      TabsModule.forRoot(),
+      AuthModule.forRoot(authConfig),
+      HttpClientModule
+    )
+  ]
+};
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
