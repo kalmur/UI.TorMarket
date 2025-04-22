9b79fe9ec9b9886370d05a8082082feaf48edcb8
ckalmar <ckalmar@craneware.com>
2025-04-22 13:57:25 +0100

    More and more signals

--- src/app/features/user-profile/components/user-profile-detail/user-profile-detail.component.ts
+++ src/app/features/user-profile/components/user-profile-detail/user-profile-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { AuthService, User } from '@auth0/auth0-angular';
-import { Observable } from 'rxjs';
+import { Component, inject, model } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../../../../core/components/nav-bar/nav-bar.component';
+import { AuthHelperService } from '../../../../core/auth/services/auth-helper.service';
+import { User } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-user-profile-detail',
@@ -12,7 +12,13 @@ import { NavBarComponent } from '../../../../core/components/nav-bar/nav-bar.com
   styleUrl: './user-profile-detail.component.scss'
 })
 export class UserProfileDetailComponent {
-  private readonly authService: AuthService = inject(AuthService);
+  private readonly authHelperService: AuthHelperService = inject(AuthHelperService);
 
-  user$: Observable<User | null | undefined> = this.authService.user$;
+  user = model<User | null | undefined>(null);
+
+  constructor() {
+    this.authHelperService.user$.subscribe((user) => {
+      this.user.set(user);
+    });
+  }
 }
