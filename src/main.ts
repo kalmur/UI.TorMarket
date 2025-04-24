904e1c1c5aaadbfbc9b4af3dc8b6b6a3a264ea95
ckalmar <ckalmar@craneware.com>
2025-04-24 10:47:58 +0100

    Adding global error handler

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
+import { ErrorHandler, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AuthModule } from '@auth0/auth0-angular';
 import { authConfig } from './app/core/auth/config/auth.config';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { GlobalErrorHandler } from './app/core/services/global-error-handler.service';
 
 const appConfig = {
   providers: [
@@ -23,7 +24,8 @@ const appConfig = {
         preventDuplicates: true,
       })
     ),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ]
 };
 
