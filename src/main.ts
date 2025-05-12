033133808fd062a1ba90727d7475df40f545233b
ckalmar <ckalmar@craneware.com>
2025-05-12 15:18:43 +0100

    Update http error handling

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { ErrorHandler, importProvidersFrom } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AuthModule } from '@auth0/auth0-angular';
 import { authConfig } from './app/core/auth/config/auth.config';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
-import { GlobalErrorHandler } from './app/core/services/global-error-handler.service';
 
 const appConfig = {
   providers: [
@@ -25,8 +24,7 @@ const appConfig = {
         closeButton: true
       })
     ),
-    provideHttpClient(withFetch()),
-    { provide: ErrorHandler, useClass: GlobalErrorHandler }
+    provideHttpClient(withFetch())
   ]
 };
 
